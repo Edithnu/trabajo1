20b166d1053d52b20c5c689f6c2796b332da0af8
Edithnu <62193138+Edithnu@users.noreply.github.com>
2023-06-30 03:27:18 -0300

    navbar

--- src/app/componente/navbar/navbar.component.ts
+++ src/app/componente/navbar/navbar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  título: string;
+  constructor(){
+    this.título = "INCUYO";
+
+  }
 
 }
